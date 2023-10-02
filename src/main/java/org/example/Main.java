c9de9ad77c04c97f4a315ae3862a4e421cd758a3
CF086327 <Cezar_Filache@epam.onmicrosoft.com>
2023-10-02 16:00:49 +0300

    added assertions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,6 @@
 package org.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-
 public class Main {
     public static void main(String[] args) {
-        WebDriver driver = new EdgeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-
-        driver.get("https://pastebin.com/");
-
-        //accepting privacy terms
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By
-                .xpath("//span[text()='AGREE']"))).click();
-
-
-        //unique identifier when using cssSelector
-        WebElement code = driver.findElement(By.cssSelector("#postform-text"));
-        //adding requested text into code field
-        code.sendKeys("Hello from WebDriver");
-
-        //opening the paste expiration dropdown and selecting requested 10 minutes time
-        WebElement pasteExpiration = driver.findElement(By.id("select2-postform-expiration-container"));
-        pasteExpiration.click();
-        driver.findElement(By.xpath("//li[text()='10 Minutes']")).click();
-
-        //adding the requested title
-        WebElement title = driver.findElement(By.id("postform-name"));
-        title.sendKeys("helloweb");
-
-        //submitting by clicking the Create New Paste button
-        WebElement createPasteInput = driver.findElement(By
-                .xpath("//button[@type='submit' and contains(text(),'Paste')]"));
-        createPasteInput.click();
-
-
-        driver.quit();
-
-
     }
 }
---
+++ src/test/java/task1.java
@@ -0,0 +1,64 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TaskOne {
+    WebDriver driver = new EdgeDriver();
+
+    @AfterTest
+    public void quitBrowser() {
+        driver.quit();
+    }
+
+    @Test
+    public void testTask1() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        driver.get("https://pastebin.com/");
+
+        //accepting privacy terms
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By
+                .xpath("//span[text()='AGREE']"))).click();
+
+
+        //unique identifier when using cssSelector
+        WebElement code = driver.findElement(By.cssSelector("#postform-text"));
+        //adding requested text into code field
+        code.sendKeys("Hello from WebDriver");
+
+        //opening the paste expiration dropdown and selecting requested 10 minutes time
+        WebElement pasteExpiration = driver.findElement(By.id("select2-postform-expiration-container"));
+        pasteExpiration.click();
+        driver.findElement(By.xpath("//li[text()='10 Minutes']")).click();
+
+        //adding the requested title
+        String addedTitle = "helloweb";
+        WebElement title = driver.findElement(By.id("postform-name"));
+        title.sendKeys(addedTitle);
+
+        //submitting by clicking the Create New Paste button
+        WebElement createPasteInput = driver.findElement(By
+                .xpath("//button[@type='submit' and contains(text(),'Paste')]"));
+        createPasteInput.click();
+        System.out.println("Test");
+        //add assertion
+        WebElement resultTitle = driver.findElement(By.cssSelector(".info-top"));
+        Assert.assertEquals(resultTitle.getText(), addedTitle, "Title is not " + addedTitle);
+
+        //failed assert example
+//        Assert.assertEquals("toFail", addedTitle, "Title is not " + addedTitle);
+
+        //verify assertion
+        System.out.println(resultTitle.getText() + " vs " + addedTitle);
+        driver.quit();
+    }
+}
